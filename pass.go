59f04243bcbb33b07d6134d82bb4b9e7a875c16a
Dylan Arts <86665189+dylan-arts@users.noreply.github.com>
2024-10-18 14:48:15 +0200

    Update pass.go

--- pass.go
+++ pass.go
@@ -210,7 +210,7 @@ func sign(w *zip.Writer, tempDir, password string) error {
 		"-sign",
 		"-signer", fmt.Sprintf("%s/certificate.pem", tempDir),
 		"-inkey", fmt.Sprintf("%s/key.pem", tempDir),
-		"-certfile", "wwdr.pem",
+		"-certfile", "/tmp/wwdr.pem",
 		"-in", fmt.Sprintf("%s/manifest.json", tempDir),
 		"-out", fmt.Sprintf("%s/signature", tempDir),
 		"-outform", "der",
