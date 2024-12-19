5367f33c61cf28ef2a1754e5ff8db5f3a3486c62
Dylan Arts <86665189+dylan-arts@users.noreply.github.com>
2024-12-19 11:30:22 +0100

    updated folder structure

--- pass.go
+++ pass.go
@@ -13,20 +13,18 @@ import (
 	"path/filepath"
 )
 
-// New will create a new Apple pass given the directory of companion files, the
-// password needed to open the certificate, and the certificate. You should read
-// the returned reader into a file, this file is your Apple pass and can be opened
-// from iOS and macOS devices.
+// New creates a new Apple pass, using a randomly generated temporary directory.
 func New(passID string, password string, cert io.Reader) (io.Reader, error) {
-	tempDir := fmt.Sprintf("/app/storage/tmp/%s", passID)
-	err := os.MkdirAll(tempDir, 0755)
+	// Create a unique temporary directory
+	tempDir, err := os.MkdirTemp("/app/storage/tmp", "pass-*")
 	if err != nil {
 		return nil, err
 	}
 	defer os.RemoveAll(tempDir)
 
 	// Copy certificate to file
-	c, err := os.Create(fmt.Sprintf("%s/certificates.p12", tempDir))
+	certFile := filepath.Join(tempDir, "certificates.p12")
+	c, err := os.Create(certFile)
 	if err != nil {
 		return nil, err
 	}
@@ -37,15 +35,11 @@ func New(passID string, password string, cert io.Reader) (io.Reader, error) {
 		return nil, err
 	}
 
-	// Create certificate.pem
-	err = pem(tempDir, password)
-	if err != nil {
+	// Extract key and cert from p12
+	if err = pem(tempDir, password); err != nil {
 		return nil, err
 	}
-
-	// Create key.pem
-	err = key(tempDir, password)
-	if err != nil {
+	if err = key(tempDir, password); err != nil {
 		return nil, err
 	}
 
@@ -54,15 +48,13 @@ func New(passID string, password string, cert io.Reader) (io.Reader, error) {
 	w := zip.NewWriter(buf)
 	defer w.Close()
 
-	// Bundle the files
-	err = bundle(w, passID, tempDir)
-	if err != nil {
+	// Bundle files from passID directory (could also be passed in or handled elsewhere)
+	if err = bundle(w, passID, tempDir); err != nil {
 		return nil, err
 	}
 
 	// Sign the manifest
-	err = sign(w, tempDir, password)
-	if err != nil {
+	if err = sign(w, tempDir, password); err != nil {
 		return nil, err
 	}
 
@@ -73,132 +65,105 @@ func key(tempDir, password string) error {
 	cmd := exec.Command(
 		"openssl",
 		"pkcs12",
-		"-in", fmt.Sprintf("%s/certificates.p12", tempDir),
+		"-in", filepath.Join(tempDir, "certificates.p12"),
 		"-nocerts",
-		"-out", fmt.Sprintf("%s/key.pem", tempDir),
+		"-out", filepath.Join(tempDir, "key.pem"),
 		"-passin", fmt.Sprintf("pass:%s", password),
 		"-passout", fmt.Sprintf("pass:%s1234", password),
 	)
-
-	_, err := cmd.CombinedOutput() // Capture both stdout and stderr
-	if err != nil {
-		return err
-	}
-	return nil
+	_, err := cmd.CombinedOutput()
+	return err
 }
 
 func pem(tempDir, password string) error {
 	cmd := exec.Command(
 		"openssl",
 		"pkcs12",
-		"-in", fmt.Sprintf("%s/certificates.p12", tempDir),
+		"-in", filepath.Join(tempDir, "certificates.p12"),
 		"-clcerts",
 		"-nokeys",
-		"-out", fmt.Sprintf("%s/certificate.pem", tempDir),
+		"-out", filepath.Join(tempDir, "certificate.pem"),
 		"-passin", fmt.Sprintf("pass:%s", password),
 	)
-
-	_, err := cmd.CombinedOutput() // Capture both stdout and stderr
-	if err != nil {
-		return err
-	}
-
-	return nil
+	_, err := cmd.CombinedOutput()
+	return err
 }
 
-// bundle will read all of the files in the passDir, create a manifest.json, and
-// add all files to the zip archive.
 func bundle(w *zip.Writer, passDir, tempDir string) error {
 	files, err := ioutil.ReadDir(passDir)
 	if err != nil {
 		return err
 	}
 
-	var m = make(map[string]string)
+	m := make(map[string]string)
 	for _, fi := range files {
-		// Skip directories, they are meaningless in this situation
 		if fi.IsDir() {
 			continue
 		}
 
-		// Open the file in the directory
 		f, err := os.Open(filepath.Join(passDir, fi.Name()))
 		if err != nil {
 			return err
 		}
+		defer f.Close()
 
-		// Create the sha writer
 		hw := sha1.New()
-
-		// Create the zip writer
 		zw, err := w.Create(fi.Name())
 		if err != nil {
 			return err
 		}
 
 		mw := io.MultiWriter(hw, zw)
-
-		// Write the file to the zip writer
-		_, err = io.Copy(mw, f)
-		if err != nil {
+		if _, err = io.Copy(mw, f); err != nil {
 			return err
 		}
 
-		// Add the hash to a map, later we will json.Marshal this to make manifest.json
 		sha := hw.Sum(nil)
 		m[fi.Name()] = fmt.Sprintf("%x", sha)
 	}
 
-	// Create the file writer
-	f, err := os.Create(filepath.Join(tempDir, "manifest.json"))
+	// Write manifest.json
+	manifestPath := filepath.Join(tempDir, "manifest.json")
+	mf, err := os.Create(manifestPath)
 	if err != nil {
 		return err
 	}
-	defer f.Close()
+	defer mf.Close()
 
-	// Create the zip writer
 	zw, err := w.Create("manifest.json")
 	if err != nil {
 		return err
 	}
+	mw := io.MultiWriter(mf, zw)
 
-	mw := io.MultiWriter(f, zw)
-
-	// Write the JSON to the file, and zip
-	err = json.NewEncoder(mw).Encode(m)
-	if err != nil {
+	if err = json.NewEncoder(mw).Encode(m); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// sign will sign the manifest json using the keys and certificates created
-// in key and pem respectively. It will then write the signature file to the zip
-// archive so that we can open the pass.
 func sign(w *zip.Writer, tempDir, password string) error {
-	// Sign the bundle
 	cmd := exec.Command(
 		"openssl",
 		"smime",
 		"-sign",
-		"-signer", fmt.Sprintf("%s/certificate.pem", tempDir),
-		"-inkey", fmt.Sprintf("%s/key.pem", tempDir),
+		"-signer", filepath.Join(tempDir, "certificate.pem"),
+		"-inkey", filepath.Join(tempDir, "key.pem"),
 		"-certfile", "/app/storage/wwdr.pem",
-		"-in", fmt.Sprintf("%s/manifest.json", tempDir),
-		"-out", fmt.Sprintf("%s/signature", tempDir),
+		"-in", filepath.Join(tempDir, "manifest.json"),
+		"-out", filepath.Join(tempDir, "signature"),
 		"-outform", "der",
 		"-binary",
 		"-passin", fmt.Sprintf("pass:%s1234", password),
 	)
 
-	_, err := cmd.CombinedOutput() // Capture both stdout and stderr <- use _ output for debug
+	_, err := cmd.CombinedOutput()
 	if err != nil {
 		return err
 	}
 
-	// Add signature to the zip
-	sig, err := os.Open(fmt.Sprintf("%s/signature", tempDir))
+	sig, err := os.Open(filepath.Join(tempDir, "signature"))
 	if err != nil {
 		return err
 	}
@@ -210,9 +175,5 @@ func sign(w *zip.Writer, tempDir, password string) error {
 	}
 
 	_, err = io.Copy(zw, sig)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
