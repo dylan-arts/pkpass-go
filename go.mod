f6586aabf9227e4e437b6d61094a47b1faefe23e
Dylan Arts <86665189+dylan-arts@users.noreply.github.com>
2024-12-19 11:55:24 +0100

    proper error handling

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/globe-protocol/pkpass-go
 
 go 1.22
+
+require github.com/globe-protocol/util-package v1.0.29
+
+require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/newrelic/go-agent/v3 v3.33.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.56.3 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- pass.go
+++ pass.go
@@ -8,9 +8,16 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/globe-protocol/util-package"
+)
+
+const (
+	pkpassCreationError = "failed to create pass"
 )
 
 // New creates a new Apple pass, using a randomly generated temporary directory.
@@ -24,7 +31,7 @@ func New(workingDir, passID, password string, cert io.Reader) (io.Reader, error)
 	// Create a unique temporary directory under the specified workingDir
 	tempDir, err := os.MkdirTemp(workingDir, "pass-*")
 	if err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	defer os.RemoveAll(tempDir)
 
@@ -32,21 +39,21 @@ func New(workingDir, passID, password string, cert io.Reader) (io.Reader, error)
 	certFile := filepath.Join(tempDir, "certificates.p12")
 	c, err := os.Create(certFile)
 	if err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	defer c.Close()
 
 	_, err = io.Copy(c, cert)
 	if err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	// Extract key and cert from p12
 	if err = pem(tempDir, password); err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	if err = key(tempDir, password); err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	// Create zip buffer
@@ -56,12 +63,12 @@ func New(workingDir, passID, password string, cert io.Reader) (io.Reader, error)
 
 	// Bundle files from the passID directory
 	if err = bundle(w, passID, tempDir); err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	// Sign the manifest
 	if err = sign(w, tempDir, password, workingDir); err != nil {
-		return nil, err
+		return nil, util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	return buf, nil
@@ -78,7 +85,7 @@ func key(tempDir, password string) error {
 		"-passout", fmt.Sprintf("pass:%s1234", password),
 	)
 	_, err := cmd.CombinedOutput()
-	return err
+	return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 }
 
 func pem(tempDir, password string) error {
@@ -92,13 +99,13 @@ func pem(tempDir, password string) error {
 		"-passin", fmt.Sprintf("pass:%s", password),
 	)
 	_, err := cmd.CombinedOutput()
-	return err
+	return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 }
 
 func bundle(w *zip.Writer, passDir, tempDir string) error {
 	files, err := ioutil.ReadDir(passDir)
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	m := make(map[string]string)
@@ -109,19 +116,19 @@ func bundle(w *zip.Writer, passDir, tempDir string) error {
 
 		f, err := os.Open(filepath.Join(passDir, fi.Name()))
 		if err != nil {
-			return err
+			return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 		}
 		defer f.Close()
 
 		hw := sha1.New()
 		zw, err := w.Create(fi.Name())
 		if err != nil {
-			return err
+			return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 		}
 
 		mw := io.MultiWriter(hw, zw)
 		if _, err = io.Copy(mw, f); err != nil {
-			return err
+			return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 		}
 
 		sha := hw.Sum(nil)
@@ -132,18 +139,18 @@ func bundle(w *zip.Writer, passDir, tempDir string) error {
 	manifestPath := filepath.Join(tempDir, "manifest.json")
 	mf, err := os.Create(manifestPath)
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	defer mf.Close()
 
 	zw, err := w.Create("manifest.json")
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	mw := io.MultiWriter(mf, zw)
 
 	if err = json.NewEncoder(mw).Encode(m); err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	return nil
@@ -166,20 +173,24 @@ func sign(w *zip.Writer, tempDir, password, wwdrCertPath string) error {
 
 	_, err := cmd.CombinedOutput()
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	sig, err := os.Open(filepath.Join(tempDir, "signature"))
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 	defer sig.Close()
 
 	zw, err := w.Create("signature")
 	if err != nil {
-		return err
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
 	}
 
 	_, err = io.Copy(zw, sig)
-	return err
+	if err != nil {
+		return util.NewErrorf(http.StatusInternalServerError, err, pkpassCreationError)
+	}
+
+	return nil
 }
