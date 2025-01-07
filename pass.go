aecebddc5e619b50e48e828d0976eecf98f681fc
Dylan Arts <86665189+dylan-arts@users.noreply.github.com>
2025-01-07 15:30:44 +0100

    Update pass.go

--- pass.go
+++ pass.go
@@ -151,12 +151,6 @@ func bundle(w *zip.Writer, passDir, tempDir string) error {
 		return err
 	}
 
-	// DEBUG
-	_, err = os.Stat(manifestPath)
-	if err != nil {
-		log.Println("file doest not exist")
-	}
-
 	// Add manifest.json to the zip
 	if err := addFileToZip(w, manifestPath, "manifest.json", nil); err != nil {
 		return err
